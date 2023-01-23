4216f5bc7dc9978091233c540287c81f28cb9bd7
Włodzimierz Kozłowski <wlodzimierz@kozlowscy.org>
2023-01-23 21:22:41 +0100

    zmiana orientacji diagramu

--- dcv.sh
+++ dcv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image -f docker-compose.yml
+docker run --rm -it --name dcv -v $(pwd):/input pmsipilot/docker-compose-viz render -m image -f --horizontal docker-compose.yml
